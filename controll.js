5c1313c899e9bf5fd24de75fe1ea95f1e6d03161
privateidaho99 <bendemor@byu.net>
2018-10-18 00:18:51 +0000

    changes

--- controll.js
+++ controll.js
@@ -2,13 +2,15 @@ angular.module('app', [])
     .controller('mainCtrl', mainCtrl)
 
 function mainCtrl($scope) {
-    $scope.amount = [];
+    $scope.amounts = [];
     $scope.addNew = function(amount) {
-        $scope.amount.push({
+        $scope.amounts.push({
             value: amount.value,
             name: amount.name,
+            budget: amount.budget,
     });
     amount.value = "";
     amount.name = "";
+    amount.budget = "";
     }
 }
